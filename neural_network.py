9c1688bee45b19cd8b83898761088062e459a469
Jason Rute <jason.rute@gmail.com>
2017-05-24 15:03:28 -0400

    Added ability to enter weights and biases into the constructor.

--- neural_network.py
+++ neural_network.py
@@ -1,21 +1,24 @@
 import numpy as np
 
 class Neural_Network():
-    def __init__(self, input, output, hidden_layer_sizes=[]):
+    def __init__(self, input, output, hidden_layer_sizes=[], weights_biases=None):
         self.input = input # number of input nodes + 1 for bias
         self.output = output # number of output nodes
 
         # Layers
-        self.layer_sizes = [self.input] + hidden_layer_sizes + [self.output]
+        self.layer_sizes = [self.input] + list(hidden_layer_sizes) + [self.output]
         self.layers = [np.ones(s) for s in self.layer_sizes]
         self.num_layers = len(self.layers) 
 
         # Weights and biases
-        self.weights = []
-        self.biases = []
-        for s0, s1 in zip(self.layer_sizes[:-1], self.layer_sizes[1:]):
-            self.weights.append(np.random.randn(s0, s1))
-            self.biases.append(np.random.randn(s1))
+        if weights_biases == None:
+            self.weights = []
+            self.biases = []
+            for s0, s1 in zip(self.layer_sizes[:-1], self.layer_sizes[1:]):
+                self.weights.append(np.random.randn(s0, s1))
+                self.biases.append(np.random.randn(s1))
+        else:
+            self.weights, self.biases = weights_biases
 
         # Lists for enumeration
         self.weights_and_biases = list(zip(range(self.num_layers - 1), self.weights, self.biases))
